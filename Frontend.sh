b0b68e8ebe4f2b5f1fbb5f16bb78e6236ab3b10f
Haseeb <Mohammad9haseeb@gmail.com>
2023-08-30 23:34:06 -0400

    First time use of Eco command

--- Frontend.sh
+++ Frontend.sh
@@ -1,14 +1,19 @@
-
+echo -e "\e[32m>>>>>>>>Install nginx<<<<<<<<\e[0m"
 yum install nginx -y
 
+echo -e "\e[32m>>>>>>>>Configure Reverse Proxy<<<<<<<<\e[0m"
 cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 
+echo -e "\e[32m>>>>>>>>Remove nginx Content<<<<<<<<\e[0m"
 rm -rf /usr/share/nginx/html/*
 
+echo -e "\e[32m>>>>>>>>Download frontend content<<<<<<<<\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
+echo -e "\e[32m>>>>>>>>Unzip frontend content<<<<<<<<\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
+echo -e "\e[32m>>>>>>>>Start nginx<<<<<<<<\e[0m"
 systemctl enable nginx
 systemctl start nginx
