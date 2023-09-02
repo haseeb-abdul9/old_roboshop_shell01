9b3096c468a85a1a2f00dcd805e9326f3b9a1a85
Haseeb <Mohammad9haseeb@gmail.com>
2023-09-02 03:30:15 -0400

    rabbitmq

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -6,10 +6,11 @@ echo -e "\e[32m>>>>>>>>Download & Install RabbitMQ server<<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
 yum install rabbitmq-server -y
 
-echo -e "\e[32m>>>>>>>>Start RabbitMQ<<<<<<<<\e[0m"
-systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
-
 echo -e "\e[32m>>>>>>>>Change default Username & password<<<<<<<<\e[0m"
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+
+echo -e "\e[32m>>>>>>>>Start RabbitMQ<<<<<<<<\e[0m"
+systemctl enable rabbitmq-server
+systemctl restart rabbitmq-server
+
