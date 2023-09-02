78ee14801182a4e7ad31f5d9ddced95c04b51d7d
Haseeb <Mohammad9haseeb@gmail.com>
2023-09-02 01:37:14 -0400

    redis port multi change 3

--- redis.sh
+++ redis.sh
@@ -6,7 +6,7 @@ dnf module enable redis:remi-6.2 -y
 yum install redis -y
 
 echo -e "\e[32m>>>>>>>>Change port<<<<<<<<\e[0m"
-sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis/redis.conf /etc/redis.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
 
 echo -e "\e[32m>>>>>>>>Start Redis<<<<<<<<\e[0m"
 systemctl enable redis
