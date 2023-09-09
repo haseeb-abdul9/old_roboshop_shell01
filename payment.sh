7348fdb8325ed50931e7c12777a5dbdd4b27fa45
Haseeb <Mohammad9haseeb@gmail.com>
2023-09-09 00:13:46 -0400

    hopefully last change

--- payment.sh
+++ payment.sh
@@ -2,7 +2,7 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 rabbitmq_password=$1
-component=${component}
+component=payment
 
 if [ -z "$rabbitmq_password" ]; then
   echo Input rabbitmq_password missing
